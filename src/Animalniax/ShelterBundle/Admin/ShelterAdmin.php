b11b81c63cf8a417fd04bf3830dc4715b0809e3d
rupert <rupert@Mr-portato>
2016-06-02 20:25:34 +0200

    Added Shelter administration

---
+++ src/Animalniax/ShelterBundle/Admin/ShelterAdmin.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Animalniax\ShelterBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+/**
+ * ShelterAdmin
+ *
+ * @author rupert
+ */
+class ShelterAdmin extends Admin
+{
+    protected $translationDomain = 'AnimalniaxShelterBundle';
+    
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+                ->add('name', 'text', [
+                    'label'=>'animalniax.shelter.fields.name',
+                    'required'=>true
+                ])
+                ->add('address', 'textarea', [
+                    'label'=>'animalniax.shelter.fields.address',
+                    'required'=>false
+                ])
+                ->add('contactPerson', 'text', [
+                    'label'=>'animalniax.shelter.fields.contact_person',
+                    'required'=>false
+                ])
+                ->add('contactPhone', 'text', [
+                    'label'=>'animalniax.shelter.fields.contact_phone',
+                    'required'=>false
+                ])
+                ;
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+                ->add('name')
+                ;
+    }
+    
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+                ->addIdentifier('id')
+                ->add('name')
+                ->add('address')
+                ->add('contactPerson')
+                ->add('contactPhone')
+                ;
+    }
+}
---
+++ src/Animalniax/ShelterBundle/Entity/Shelter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Animalniax\ShelterBundle\Entity;
+
+/**
+ * Shelter
+ *
+ * @author rupert
+ */
+class Shelter
+{
+    protected $id;
+    
+    protected $name;
+    
+    protected $address;
+    
+    protected $contactPerson;
+    
+    protected $contactPhone;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function getContactPerson()
+    {
+        return $this->contactPerson;
+    }
+
+    public function getContactPhone()
+    {
+        return $this->contactPhone;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    public function setContactPerson($contactPerson)
+    {
+        $this->contactPerson = $contactPerson;
+    }
+
+    public function setContactPhone($contactPhone)
+    {
+        $this->contactPhone = $contactPhone;
+    }
+}
